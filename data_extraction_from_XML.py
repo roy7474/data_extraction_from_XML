37d35f9ff9c7d696cb0a905421388fb48485d502
Roy Contreras Benitez <roy7474@gmail.com>
2023-10-23 18:34:35 -0400

    methods to ignored url error have been added to the program

--- data_extraction_from_XML.py
+++ data_extraction_from_XML.py
@@ -80,4 +80,11 @@ Sample Execution
 
 
 '''
-
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+import re
+#ignore url error
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
